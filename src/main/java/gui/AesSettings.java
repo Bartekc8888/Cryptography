dfca1906a0104605e86ffa364dbc04cf4b1464c3
Bartłomiej Ciesielski <bartekc8@op.pl>
2018-11-01 22:15:27 +0100

    Implement some ui methods

--- src/main/java/gui/AesSettings.java
+++ src/main/java/gui/AesSettings.java
@@ -1,13 +1,29 @@
 package gui;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import aes.AESVersion;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 import org.controlsfx.control.ToggleSwitch;
 
-public class AesSettings {
-    public ComboBox versionComboBox;
+public class AesSettings implements Initializable {
+    public ComboBox<AESVersion> versionComboBox;
     public TextField passwordField;
 
     public TextField inputFileField;
@@ -22,21 +38,41 @@ public class AesSettings {
     public Button generatePasswordButton;
     public Button calculateButton;
     public Button cancelButton;
+    public AnchorPane AESTabAnchor;
+
+    private Window currentWindow;
+
+    private static final String alphaNumericChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static SecureRandom random = new SecureRandom();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initVersionComboBox();
+    }
 
     public void onModeToggle(MouseEvent mouseEvent) {
 
     }
 
     public void onInputPathButtonClick(MouseEvent mouseEvent) {
+        File selectedDirectory = getFileChooser().showOpenDialog(currentWindow);
 
+        if (selectedDirectory != null) {
+            inputFileField.setText(selectedDirectory.getAbsolutePath());
+        }
     }
 
     public void onOutputPathButtonClick(MouseEvent mouseEvent) {
+        File selectedDirectory = getFileChooser().showSaveDialog(currentWindow);
 
+        if (selectedDirectory != null) {
+            outputFileField.setText(selectedDirectory.getAbsolutePath());
+        }
     }
 
     public void onGenerateClick(MouseEvent mouseEvent) {
-
+        String randomString = randomString();
+        passwordField.setText(randomString);
     }
 
     public void onCalculateClick(MouseEvent mouseEvent) {
@@ -44,6 +80,36 @@ public class AesSettings {
     }
 
     public void onCancelClick(MouseEvent mouseEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
 
+    private FileChooser getFileChooser() {
+        if (currentWindow == null) {
+            currentWindow = AESTabAnchor.getScene().getWindow();
+        }
+
+        FileChooser directoryChooser = new FileChooser();
+        File home = FileSystemView.getFileSystemView().getHomeDirectory();
+        directoryChooser.setInitialDirectory(home);
+
+        return directoryChooser;
+    }
+
+    private void initVersionComboBox() {
+        List<AESVersion> aesVersions = Arrays.asList(AESVersion.values());
+        ObservableList<AESVersion> observableList = FXCollections.observableList(aesVersions);
+        versionComboBox.setItems(observableList);
+        versionComboBox.setValue(AESVersion.AES_256);
+    }
+
+    private String randomString() {
+        int length = 32;
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(alphaNumericChars.charAt(random.nextInt(alphaNumericChars.length())));
+        }
+        return sb.toString();
     }
 }
--- src/main/java/gui/AlgorithmSettings.java
+++ src/main/java/gui/AlgorithmSettings.java
@@ -2,12 +2,15 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
@@ -17,10 +20,19 @@ public class AlgorithmSettings implements Initializable {
 
     @FXML
     public Pane settingsPane;
+    public ToggleButton AESTabButton;
+    public ToggleButton ElGamalTabButton;
+    public ToggleButton DSATabButton;
+    public ToggleGroup AlgorithmSettingsTabButtonGroup;
+
+    private String previousTab;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        AESTabButton.setSelected(true);
         setScreen(AES_SETTINGS_PANE_URI);
+
+        disallowUnselectingAllToggleButtons();
     }
 
     public void OpenDsaScreen(MouseEvent mouseEvent) {
@@ -36,6 +48,10 @@ public class AlgorithmSettings implements Initializable {
     }
 
     private void setScreen(String uri) {
+        if (Objects.equals(previousTab, uri)) {
+            return;
+        }
+
         try {
             URL resource = getClass().getClassLoader().getResource(uri);
             if (resource == null) {
@@ -45,8 +61,17 @@ public class AlgorithmSettings implements Initializable {
             Parent settingsScreen = FXMLLoader.load(resource);
             settingsPane.getChildren().removeAll();
             settingsPane.getChildren().setAll(settingsScreen);
+
+            previousTab = uri;
         } catch (IOException e) {
             throw new RuntimeException("Settings fxml initialization error.", e);
         }
+    }
+
+    private void disallowUnselectingAllToggleButtons() {
+        AlgorithmSettingsTabButtonGroup.selectedToggleProperty().addListener((obsVal, oldVal, newVal) -> {
+            if (newVal == null)
+                oldVal.setSelected(true);
+        });
     }
 }
